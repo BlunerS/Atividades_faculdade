fb992449862b0a9c66b8433f8afa97e1a895e145
BlunerS <71131510+BlunerS@users.noreply.github.com>
2022-08-03 10:21:55 -0300

    2022.08.03.01

---
+++ aula2desafio.py
@@ -0,0 +1,13 @@
+def pular_nuvens(nuvens):
+    saltos = 0
+    indice = 0
+    while indice < (len(nuvens) - 2):
+        if nuvens[indice + 2] != 1:
+            # 1
+            # 2
+        else:
+            # 3
+            # 4
+    return saltos
+
+print(pular_nuvens(nuvens))
